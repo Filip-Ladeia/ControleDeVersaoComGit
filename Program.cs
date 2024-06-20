b765be80888b759e0038f60618cab07bc0313c84
Filipe <soaresfilipe926@gmail.com>
2024-06-20 16:24:55 -0300

    Calculadora atualizada!

--- Program.cs
+++ Program.cs
@@ -1,61 +1,75 @@
-﻿
+﻿using System;
 
-using System;
-using System.Net.Mail;
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo à calculadora!");
 
-namespace Calculadora;
+            Console.WriteLine("Digite uma operação a ser feita!");
+            Console.WriteLine("1 - SOMAR");
+            Console.WriteLine("2 - SUBTRAIR");
+            Console.WriteLine("3 - MULTIPLICAR");
+            Console.WriteLine("4 - DIVIDIR");
 
-class Program{
-    static void Main(string[] args) {
+            string resp = Console.ReadLine();
 
-        Console.WriteLine("Bem vindo a calculadora!");
-
-        Console.WriteLine("Digite uma operação a ser feita!");
-        Console.WriteLine("1 SOMAR");
-        Console.WriteLine("2 SUBTRAIR");
-        Console.WriteLine("3 MULTIPLICADOR");
-        Console.WriteLine("4 DIVIDIR");
-
-        string resp = Console.ReadLine();
-
-        if (resp == "1") {
-            Console.WriteLine("Digite os valores a ser somados:");
-            double valor1 = double.Parse(Console.ReadLine());
-            double valor2 = double.Parse(Console.ReadLine());
-            double soma = valor1 + valor2;
-            Console.WriteLine("A soma é: " + soma);
+            switch (resp)
+            {
+                case "1":
+                    Console.WriteLine("Digite os valores a serem somados:");
+                    double[] valoresSoma = LerValores();
+                    Console.WriteLine("A soma é: " + (valoresSoma[0] + valoresSoma[1]));
+                    break;
+                case "2":
+                    Console.WriteLine("Digite os valores a serem subtraídos:");
+                    double[] valoresSubtracao = LerValores();
+                    Console.WriteLine("A subtração é: " + (valoresSubtracao[0] - valoresSubtracao[1]));
+                    break;
+                case "3":
+                    Console.WriteLine("Digite os valores a serem multiplicados:");
+                    double[] valoresMultiplicacao = LerValores();
+                    Console.WriteLine("A multiplicação é: " + (valoresMultiplicacao[0] * valoresMultiplicacao[1]));
+                    break;
+                case "4":
+                    Console.WriteLine("Digite os valores a serem divididos:");
+                    double[] valoresDivisao = LerValores();
+                    if (valoresDivisao[1] != 0)
+                    {
+                        Console.WriteLine("A divisão é: " + (valoresDivisao[0] / valoresDivisao[1]));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Erro: Divisão por zero não é permitida.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Operação inválida.");
+                    break;
+            }
         }
 
-        if (resp == "2") {
-          Console.WriteLine("Digite os valores a ser somados:");
-          double val1 = double.Parse(Console.ReadLine());
-          double val2 = double.Parse(Console.ReadLine());
-          double sm = val1 - val2;
-            Console.WriteLine("A soma é: " + sm);
-
+        static double[] LerValores()
+        {
+            double valor1, valor2;
+            while (true)
+            {
+                try
+                {
+                    valor1 = double.Parse(Console.ReadLine());
+                    valor2 = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Entrada inválida. Digite novamente os valores.");
+                }
+            }
+            return new double[] { valor1, valor2 };
         }
-
-        if  (resp == "3") {
-            Console.WriteLine("Digite os valores a ser somados:");
-            double valr1 = double.Parse(Console.ReadLine());
-            double valr2 = double.Parse(Console.ReadLine());
-            double sm = valr1 * valr2;
-            Console.WriteLine("A soma é: " + sm);
-
-        }
-
-        if (resp == "4") {
-           Console.WriteLine("Digite o valor a ser somados:");
-           double vl1 = double.Parse(Console.ReadLine());
-           double vl2 = double.Parse(Console.ReadLine());
-           double sm = vl1 / vl2;
-           Console.WriteLine("A soma é:" + sm);
-
-        }
-
     }
 }
-
-
 
 
